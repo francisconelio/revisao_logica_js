e4730a4fe6afb2b04e8f57bbaf1800d8e4bb16cb
Francisco Nélio <nelio.romero.fn@gmail.com>
2023-02-27 18:59:08 -0300

    revisao_logica_js

---
+++ script.js
@@ -0,0 +1,10 @@
+var n1 = parseFloat(prompt("Insira nota 1");
+var n2 = parseFloat(prompt("Insira nota 2");
+var n3 = parseFloat(prompt("Insira nota 3");
+var n4 = parseFloat(prompt("Insira nota 4");
+// Fazendo uma chamada na função media()
+media(); // Saída: 6.75
+// Função para Calcular a média das 4 variáveis
+function media() {
+   console.log((n1 + n2 + n3 + n4) / 4);
+}
